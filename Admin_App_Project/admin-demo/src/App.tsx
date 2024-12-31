797c57830c2316e3335ba1723e20b56c6bbfab99
singhritik08 <singhritik0810@gmail.com>
2024-12-31 16:33:41 +0530

    server link added

--- Admin_App_Project/admin-demo/src/App.tsx
+++ Admin_App_Project/admin-demo/src/App.tsx
@@ -7,7 +7,7 @@ import { CategoryList } from "./components/categoryList";
 import { CategoryEdit } from "./components/categoryEdit";
 import { CategoryCreate } from "./components/categoryCreate";
 
-const dataProvider = jsonServerProvider("http://localhost:3000")
+const dataProvider = jsonServerProvider("https://admin-demo-aiwz.onrender.com")
 const App = () => <Admin dataProvider={dataProvider}>
 <Resource name="posters" list={PosterList} edit={PosterEdit} create={PosterCreate}/>
 <Resource name="categories" list={CategoryList}  edit={CategoryEdit} create={CategoryCreate}/>
